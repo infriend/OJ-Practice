6009e878fdfb759e036d128b9c231875549c916e
infriend <stive0118@sina.com>
2022-02-21 14:50:26 +0800

    1-11.cpp

--- OJNJUSoft/ProblemSet1/1-11.cpp
+++ OJNJUSoft/ProblemSet1/1-11.cpp
@@ -1,3 +1,26 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
+struct student{
+    int p, c, m;
+    bool operator<(student s){
+        return p-s.p < 0 || (p-s.p == 0 && c-s.c > 0) || (p-s.p == 0 && c-s.c == 0 && m - s.m < 0);
+    }
+};
+
+int main(){
+    int t, n;
+    student s[10100];
+    cin >> t;
+    for (int i = 0; i < t; i++){
+        cin >> n;
+        for(int j = 0; j < n; j++){
+            cin >> s[j].p >> s[j].c >> s[j].m;
+        }
+        sort(s, s+n);
+        for(int j = 0; j < n; j++){
+            cout << s[j].p << " " << s[j].c << " " << s[j].m << endl;
+        }
+    }
+    return 0;
+}
